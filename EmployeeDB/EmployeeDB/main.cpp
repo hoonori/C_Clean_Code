b337aa4965c2eedb8c520f72739790031bac46b8
unknown <hoonori97@gmail.com>
2022-02-14 14:52:07 +0900

    [COMMON] Added basic data structure (employee)
    Signed-off-by: unknown <hoonori97@gmail.com>

--- EmployeeDB/EmployeeDB/main.cpp
+++ EmployeeDB/EmployeeDB/main.cpp
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
 
 int main() {
 
 	//printf("hello world!\n");
 	return 0;
 }
+
+
+struct Employee {
+	char employeeNum[9];
+	char name[15];
+	char cl[4];
+	char phoneNum[14];
+	char birthday[9];
+	char certi[4];
+};
+
+
+vector<Employee> memberList;
