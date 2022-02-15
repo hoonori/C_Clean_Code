c1e97b3a8e88c89633f67c43d3d6ecd67b07248c
DESKTOP-J6TV5QE\yoonj <yoonjs3017@gmail.com>
2022-02-15 10:29:48 +0900

    implement parser

--- EmployeeDB/Test_Project/test.cpp
+++ EmployeeDB/Test_Project/test.cpp
@@ -1,6 +1,5 @@
 #include "pch.h"
-#include "../EmployeeDB/main.h"
-
+#include "../EmployeeDB/Parser.cpp"
 /*
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
@@ -8,3 +7,12 @@ TEST(TestCaseName, TestName) {
 }
 
 */
+
+TEST(TestCaseName, ParseTest) {
+	Parser parser;
+
+	string str = "ADD, , , ,15123099,VXIHXOTH JHOP,CL3,010-3112-2609,19771211,ADV";
+	int res = parser.Parse(str);
+	EXPECT_EQ(res, 0);
+}
+
