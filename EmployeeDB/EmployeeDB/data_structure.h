73a1954a1ca1b17a31172ab547dd8358d53a34cd
김지훈 <55314488+hoonori@users.noreply.github.com>
2022-02-15 11:10:30 +0900

    Separate data structures to another file
    due to link issues, i think data structure should be separated to another file.

---
+++ EmployeeDB/EmployeeDB/data_structure.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <map>
+#include <string>
+using namespace std;
+
+struct Employee memberList[100000];
+
+multimap<string, int> employeeNum_map;
+multimap<string, int> name_map;
+multimap<string, int> cl_map;
+multimap<string, int> phoneNum_map;
+multimap<string, int> birthday_map;
+multimap<string, int> certi_map;
--- EmployeeDB/EmployeeDB/main.h
+++ EmployeeDB/EmployeeDB/main.h
@@ -6,19 +6,6 @@
 
 using namespace std;
 
-
-struct Employee {
-	int valid;
-	string employeeNum;
-	string name;
-	string cl;
-	string phoneNum;
-	string birthday;
-	string certi;
-};
-
-struct Employee memberList[100000];
-
 multimap<string, int> employeeNum_map;
 multimap<string, int> name_map;
 multimap<string, int> cl_map;
