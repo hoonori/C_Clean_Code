addfa9fdcc7507fefdd8a41b2083418d7fa8bf4e
JunsukYoon <yoonjs3017@gmail.com>
2022-02-15 20:04:39 +0900

    add DataBase Fail TCs
    Signed-off-by: JunsukYoon <yoonjs3017@gmail.com>

--- EmployeeDB/Test_Project/DataBaseTest.cpp
+++ EmployeeDB/Test_Project/DataBaseTest.cpp
@@ -1,5 +1,21 @@
 #include "pch.h"
 
-TEST(DataBaseTest, TestName) {
+TEST(DataBaseTest, CreateRecordTest) {
+	EXPECT_TRUE(false);
+}
+
+TEST(DataBaseTest, ReadRecordTest) {
+	EXPECT_TRUE(false);
+}
+
+TEST(DataBaseTest, UpdateRecordTest) {
+	EXPECT_TRUE(false);
+}
+
+TEST(DataBaseTest, DeleteRecordTest) {
+	EXPECT_TRUE(false);
+}
+
+TEST(DataBaseTest, FindAllTest) {
 	EXPECT_TRUE(false);
 }
