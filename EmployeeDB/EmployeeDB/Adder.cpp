7c57e32ab70483cf516e81a4f491e6dad6036b59
JunsukYoon <yoonjs3017@gmail.com>
2022-02-15 14:25:48 +0900

    adder fail TC

---
+++ EmployeeDB/EmployeeDB/Adder.cpp
@@ -0,0 +1,7 @@
+#include "Adder.h"
+
+#include "data_structure.h"
+
+void Adder::Add(string employeeNum, string name, string cl, string phoneNum, string birthday, string certi) {
+
+}
--- EmployeeDB/Test_Project/test.cpp
+++ EmployeeDB/Test_Project/test.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "../EmployeeDB/Parser.cpp"
+#include "../EmployeeDB/Adder.cpp"
+#include"../EmployeeDB/data_structure.h"
 /*
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
@@ -16,3 +18,17 @@ TEST(TestCaseName, ParseTest) {
 	EXPECT_EQ(res, 0);
 }
 
+TEST(TestCaseName, AddTest) {
+	Adder adder;
+
+	string employeeNum = "15123099";
+	string name = "VXIHXOTH JHOP";;
+	string cl = "CL3";
+	string phoneNum = "010 - 3112 - 2609";
+	string birthday = "19771211";
+	string certi = "ADV";
+
+	adder.Add(employeeNum, name, cl, phoneNum, birthday, certi);
+
+	EXPECT_TRUE(false);
+}
