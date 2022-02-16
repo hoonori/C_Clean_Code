bd6e91cb34f1cbefd199e8c54ca2b9d6d6859e0e
noh0249 <noh0249@gmail.com>
2022-02-16 10:45:35 +0900

    MOD: add mod func and test code

---
+++ EmployeeDB/EmployeeDB/data_structure.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <map>
+#include <string>
+#include <iostream>
+#define MAXTYPE 6
+#define MAX_MEMBER 100000
+using namespace std;
+
+
+struct Employee {
+	int valid;
+	string employeeNum;
+	string name;
+	string cl;
+	string phoneNum;
+	string birthday;
+	string certi;
+};
+
+enum datatype {
+	EMPLOYEENUM,
+	NAME,
+	CL,
+	PHONENUM,
+	BIRTHDAY,
+	CERTI
+};
+
+struct Employee memberList[MAX_MEMBER];
+
+multimap<string, int> employeeNum_map;
+multimap<string, int> name_map;
+multimap<string, int> cl_map;
+multimap<string, int> phoneNum_map;
+multimap<string, int> birthday_map;
+multimap<string, int> certi_map;
+
+int currentIndex = 0;
