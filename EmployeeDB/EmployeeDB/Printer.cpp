f7df8aab5b100ba6ab8bc35cfddb70bc28fda0fe
JunsukYoon <yoonjs3017@gmail.com>
2022-02-16 11:09:17 +0900

    add Printer Fail TC

---
+++ EmployeeDB/EmployeeDB/Printer.cpp
@@ -0,0 +1,17 @@
+#include "Printer.h"
+
+Printer::Printer(ofstream* ofs) {
+	m_outfileStream = ofs;
+}
+
+void Printer::PrintRecord(string cmd, string employNum, string name, string cl, string phoneNum, string birthday, string certi) {
+
+}
+
+void Printer::PrintCount(string cmd, int recordCount) {
+
+}
+
+void Printer::PrintNone(string cmd) {
+
+}
